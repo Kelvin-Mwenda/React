8d53ee11d00da49e5715d65a171855fb326aa543
Kelvin <kelvinmwendadoreen@gmail.com>
2024-12-03 23:47:02 +0300

    to do list...

---
+++ ToDo-List/todo-list/src/App.jsx
@@ -0,0 +1,13 @@
+import AddItem from './components/AddItem'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AddItem/>
+    </>
+  )
+}
+
+export default App
---
+++ ToDo-List/todo-list/src/components/AddItem.jsx
@@ -0,0 +1,15 @@
+
+
+export default function AddItem() {
+    return (
+        <>
+            <div className="adddiv">
+                <h1>Add Task</h1>
+                <div className="addForm">
+                    <input type="text" placeholder="Add Task" />
+                    <button type="button">Add Task</button>
+                </div>
+            </div>
+        </>
+    )
+}
