35a7ee4bee575f782176bd491537e2142c65455a
Kelvin <kelvinmwendadoreen@gmail.com>
2024-12-07 15:15:36 +0300

    choco weather app...

---
+++ weather-app/src/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header(){
+    return (
+            <>
+                <div className="header">
+                    <div className="app-logo">
+                        <img src="./src/assets/choco_cloud.jpg" alt="List Icon" className="logo" />
+                        <h2>Choco <span style={{color:'#10a702'}}>Weather App</span></h2>
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#task">Task</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </>
+        )
+}
