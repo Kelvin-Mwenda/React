c6ac584c1c62dd1d4f17b61b8f499010d537208f
Kelvin <kelvinmwendadoreen@gmail.com>
2024-12-21 13:47:55 +0300

    API resolutions, overall outlay update...

---
+++ infinity-scroll-list/src/App.jsx
@@ -0,0 +1,18 @@
+import InfiniteList from './InfiniteList'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <Header/>
+      <InfiniteList/>
+    </div>
+    </>
+  )
+}
+
+export default App
