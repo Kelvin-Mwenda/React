a6f4b5284e23c39a09573bdfd7b2ddfb454c9af4
Kelvin <kelvinmwendadoreen@gmail.com>
2025-02-16 03:02:10 +0300

    Quotes update...

--- quotes_/src/Quote.jsx
+++ quotes_/src/Quote.jsx
@@ -8,20 +8,37 @@ function Quote(){
 
 
   const fetchQuote = async () => {
-    //set loading state and clear any previous errors
+    // Read the API key from your .env file
+    const apiKey = import.meta.env.VITE_APP_ID;
+
+    // Set loading state and clear any previous errors
     setLoading(true);
     setError(null);
-    //fetch a new quote and update state
+
     try {
-      // Fetch random quote from Quotable API
-      const response = await fetch('https://api.quotable.io/random');
-      // Check if response is OK (status in the range 200-299)
+      // Make a GET request to the FavQs Quote of the Day API
+      const response = await fetch('/api/qotd', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          // The FavQs API expects the API key in the format: "Token token=YOUR_API_KEY"
+          'Authorization': `Token token=${apiKey}`
+        }
+      });
+
+      console.log('API KEY:', import.meta.env.VITE_APP_ID);
+
+
+      // Check if the response is successful (status in the range 200-299)
       if (!response.ok) {
         throw new Error('Failed to fetch quote');
       }
+
       // Parse the JSON data from the response
       const data = await response.json();
-      // Update the quote state with fetched data
+      console.log(data)
+
+      // Update the quote state with the fetched data
       setQuote(data);
     } catch (err) {
       // If an error occurs, set the error state with the error message
@@ -30,7 +47,8 @@ function Quote(){
       // Once done (whether successful or not), set loading to false
       setLoading(false);
     }
-  }
+};
+
 
   // useEffect hook to fetch a quote when the component mounts
   useEffect(() => {
@@ -46,8 +64,8 @@ function Quote(){
       loading ? (<p>Loading...</p>) : error? (<p className="error">Error: {error}</p>) : (//When quote is available, display it 
       quote && (
           <>
-            <p className="quote-text">{quote.content}</p>
-            <p className="quote-author">- {quote.author}</p>
+            <h3 className="quote-text">{quote.quote.body}</h3>
+            <p className="quote-author">- {quote.quote.author}</p>
           </>
         )
       )
---
+++ slideshow/data/slideImages.js
@@ -0,0 +1,22 @@
+export const slideImages = [
+  {
+    url:'./public/Images/puppy1.jpg',
+    caption: 'Puppy 1'
+  },
+  {
+    url:'./public/Images/puppy2.jpg',
+    caption: 'Puppy 2'
+  },
+  {
+    url:'./public/Images/puppy3.jpg',
+    caption: 'Puppy 3'
+  },
+  {
+    url:'./public/Images/puppy4.jpg',
+    caption: 'Puppy 4'
+  },
+  {
+    url:'./public/Images/puppy5.jpg',
+    caption: 'Puppy 5'
+  },
+];
